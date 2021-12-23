53947e92f92129716991759e42e1c8a61a04544a
Ravender91 <zcoolz10@naver.com>
2021-12-23 22:17:09 +0900

    second modify

--- Baseball_Boilerplate/Baseball_Boilerplate/index.js
+++ Baseball_Boilerplate/Baseball_Boilerplate/index.js
@@ -10,7 +10,7 @@ gameStartButton.addEventListener('click', function () {
 })
 //input number event
 inputNumber.addEventListener('onkeydown', enterkey)
-
+//enter키 이벤트
 function enterkey () {
     if (window.event.keyCode == 13) {
         if (inputNumber.value.length > 3) {
@@ -20,8 +20,8 @@ function enterkey () {
         let strikeCount = 0;
         let ballCount = 0;
         let outCount = 0;
+        let gameCount = 0;
         
-
         for (let i = 0; i < 3; i++) {
             if (answer.toString()[i] === inputNumber.value[i]) {
                 strikeCount++;
@@ -35,20 +35,34 @@ function enterkey () {
 
         for (let i = 0; i < 3; i++) {
             for (let j = 0; j < 3; j++) {
-                
                 if (answer.toString()[i] === inputNumber.value[j]) {
                     ballCount++;
                 } else if (ballCount !== 0) {
                     outCount = 0;
-                } 
+                }
             }
         }
-        let result2 = ("Strike: " + strikeCount + " " + "Ball: " + ballCount + " " + "Out: " + outCount + " " + "InputNumber: " + inputNumber.value)
+        
+        //정답 일 때와 오답일 때
+        if (strikeCount === 3 && inputNumber.value !== '') {
+            alert("정답입니다!")
+            inputNumber.value = '';
+            gameCount = 0;
+            result2 = ''
+        } else if (strikeCount !== 3) { //답이 틀리면
+            gameCount += 1;
+            if (gameCount > 10) {
+                alert("게임오버! 정답은 " + answer);
+                inputNumber.value = '';
+                gameCount = 0;
+            }
+        }
+        console.log(gameCount)
+        // enter키 누를 때마다 이닝 수 증가.
 
-        return result.innerHTML = result2;
 
+        let result2 = (gameCount + "회" + " " + "[Your Number]: " + inputNumber.value + " " + "Strike: " + strikeCount + " " + "Ball: " + ballCount + " " + "Out: " + outCount)
+
+        return result.innerHTML = result.innerHTML + result2 + "</br>";
     }
-    
 }
-
-// "Strike: " + strikeCount + "Ball: " + ballCount + "Out: " + outCount
