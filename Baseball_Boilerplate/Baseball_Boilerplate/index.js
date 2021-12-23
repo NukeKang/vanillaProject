5428dd885982cc3695747495ae02f3ee486a0d30
Ravender91 <zcoolz10@naver.com>
2021-12-24 01:53:09 +0900

    third modify

--- Baseball_Boilerplate/Baseball_Boilerplate/index.js
+++ Baseball_Boilerplate/Baseball_Boilerplate/index.js
@@ -1,68 +1,69 @@
 let gameStartButton = document.querySelector('.game_start');
 let inputNumber = document.querySelector('.input_number');
-
 //make three digit number
-let answer = ''
-
-gameStartButton.addEventListener('click', function () {
+let answer = '';
+function makeNum () {
     answer = Math.floor(Math.random() * (1000 - 100) + 100);
-    console.log(answer);
-})
+};
+//확인용
+makeNum();
+console.log(answer);
+
+
+
+// click game start button!
+gameStartButton.addEventListener('click', makeNum);
 //input number event
-inputNumber.addEventListener('onkeydown', enterkey)
-//enter키 이벤트
+inputNumber.addEventListener('onkeydown', enterkey);
+//enter 치면 실행되는 함수
 function enterkey () {
     if (window.event.keyCode == 13) {
-        if (inputNumber.value.length > 3) {
-            alert("세자리 숫자를 입력해주세요.")
-        }
-        // 각 자릿수마다 비교. strike, ball, out 개수 구하기.
+        // 각 자릿수마다 비교. strike, ball 개수 구하기.
         let strikeCount = 0;
         let ballCount = 0;
-        let outCount = 0;
-        let gameCount = 0;
         
+
+        // strike와 ball 판정.
         for (let i = 0; i < 3; i++) {
             if (answer.toString()[i] === inputNumber.value[i]) {
                 strikeCount++;
                 ballCount--;
-            } else if (strikeCount !== 0) {
-                outCount = 0;
-            } else {
-                outCount++;
-            }
-        }
+            };
+        };
 
         for (let i = 0; i < 3; i++) {
             for (let j = 0; j < 3; j++) {
                 if (answer.toString()[i] === inputNumber.value[j]) {
                     ballCount++;
-                } else if (ballCount !== 0) {
-                    outCount = 0;
-                }
-            }
+                };
+            };
+        };
+
+        let gameCount = 0;
+        if (answer !== inputNumber.value) {
+           gameCount=gameCount + 1
         }
+        console.log(gameCount);
+
+        let scoreBoard = ("[Your Number]: " + inputNumber.value + " " + "Strike: " + strikeCount + " " + "Ball: " + ballCount);
         
-        //정답 일 때와 오답일 때
-        if (strikeCount === 3 && inputNumber.value !== '') {
-            alert("정답입니다!")
+        //정답 일 때,
+        if (answer == inputNumber.value) {
+            result.innerHTML = ("정답입니다!")
             inputNumber.value = '';
             gameCount = 0;
-            result2 = ''
-        } else if (strikeCount !== 3) { //답이 틀리면
-            gameCount += 1;
+        } else { //답이 틀리면
+            result.innerHTML += scoreBoard + "</br>";
             if (gameCount > 10) {
                 alert("게임오버! 정답은 " + answer);
                 inputNumber.value = '';
                 gameCount = 0;
             }
-        }
-        console.log(gameCount)
-        // enter키 누를 때마다 이닝 수 증가.
-
-
-        let result2 = (gameCount + "회" + " " + "[Your Number]: " + inputNumber.value + " " + "Strike: " + strikeCount + " " + "Ball: " + ballCount + " " + "Out: " + outCount)
-
-        return result.innerHTML = result.innerHTML + result2 + "</br>";
-    }
-}
+        };
+        // 세자리 이상 입력하면 알림창 뜨기
+        if (inputNumber.value.length > 3) {
+            alert('세자리 숫자를 입력해주세요.');
+            result.innerHTML = '';
+        };
+    };
+};
